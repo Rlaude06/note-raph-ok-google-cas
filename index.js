58b6ca4e937d67f1e5fb5b7451812df21009404e
Rlaude06 <43902216+Rlaude06@users.noreply.github.com>
2020-10-08 19:51:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {actionssdk, Image} = require('actions-on-google');
+
+const app = actionssdk({debug: true});
+
+app.intent('actions.intent.MAIN', (conv) => {
+    conv.ask('Salut!');
+});
+
+app.intent('actions.intent.TEXT', handleTextIntent);
+
+function handleTextIntent(conv, input) {
+    if (input === 'Allume la lampe') {
+        conv.ask('Comme si c\'était fait!');
+    } else {
+        conv.ask('Désolé mais je n\'ai pas compris.');
+    }
+}
+
+express().use(bodyParser.json(), app).listen(3000)
